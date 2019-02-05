d2c080c16b94e635fc250086e8bdd189806a8add
Yousseftarekk <yousseftarekk1@hotmail.com>
2019-02-05 23:42:16 +0200

    Thermonitorr

--- app/src/main/java/com/example/thermonitor/ListActivity.java
+++ app/src/main/java/com/example/thermonitor/ListActivity.java
@@ -4,65 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
-            TextView textView;
-            TextView textView2;
-            TextView textView3;
-            TextView textView4;
-            TextView textView5;
+            String items[]= new String[] {"Android","iPhone","Windows","Linux","BlackBerry"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
-        textView = (TextView) findViewById(R.id.textView);
-        textView2 = (TextView) findViewById(R.id.textView2);
-        textView3 = (TextView) findViewById(R.id.textView3);
-        textView4 = (TextView) findViewById(R.id.textView4);
-        textView5 = (TextView) findViewById(R.id.textView5);
-
-        textView.setOnClickListener(new View.OnClickListener() {
+        ListView listView=(ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent x= new Intent(ListActivity.this,DeviceDetailedActivity.class);
-                startActivity(x);
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i= new Intent(ListActivity.this,DeviceDetailedActivity.class);
+                startActivity(i);
             }
         });
-        textView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent x= new Intent(ListActivity.this,DeviceDetailedActivity.class);
-                startActivity(x);
 
-            }
-        });
-        textView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent x= new Intent(ListActivity.this,DeviceDetailedActivity.class);
-                startActivity(x);
 
-            }
-        });
-        textView4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent x= new Intent(ListActivity.this,DeviceDetailedActivity.class);
-                startActivity(x);
-
-            }
-        });
-        textView5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent x= new Intent(ListActivity.this,DeviceDetailedActivity.class);
-                startActivity(x);
-
-            }
-        });
     }
 }
